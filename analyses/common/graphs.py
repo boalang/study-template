9c35d91eef58e880b27fe9c3482849b196cd03f5
Robert Dyer <rdyer@unl.edu>
2022-06-24 09:39:02 -0500

    allow overriding plot settings

--- analyses/common/graphs.py
+++ analyses/common/graphs.py
@@ -18,8 +18,11 @@ from matplotlib import pyplot as plt
 
 __all__ = ["setup_plots"]
 
-def setup_plots():
+def setup_plots(rcParams = None):
     plt.rcParams['figure.figsize'] = [6.0, 5.0]
     plt.rcParams['figure.dpi'] = 600.0
     plt.rcParams['font.size'] = 24
+    if rcParams:
+        for (k,v) in rcParams.items():
+            plt.rcParams[k] = v
     plt.subplots(constrained_layout=True)
