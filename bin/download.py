13a331eb320980d4f4ee97078c5d5388ef581891
Robert Dyer <rdyer@unl.edu>
2022-07-07 11:16:38 -0500

    check downloaded file hashes

--- bin/download.py
+++ bin/download.py
@@ -15,10 +15,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 from boaapi.status import CompilerStatus, ExecutionStatus
-from utilities import *
+from hashlib import md5
 from pathlib import Path
+from utilities import *
 
 def run_query(target):
     logger.info('Running query again...')
@@ -27,7 +27,7 @@ def run_query(target):
     query, sha256 = prepare_query(target)
     job = client.query(query, get_dataset(target))
 
-    logger.debug(f'Job {job.id} is running.')
+    logger.debug(f'Job {job.id} is running...')
     job.wait()
     logger.debug(f'Job {job.id} is complete.')
 
@@ -43,9 +43,9 @@ def run_query(target):
 
     update_query_data(target, job.id, sha256)
 
-    outputPath = Path(TXT_ROOT, target)
-    if outputPath.exists():
-        outputPath.unlink()
+    target_path = Path(TXT_ROOT, target)
+    if target_path.exists():
+        target_path.unlink()
 
 def download_query(target):
     logger.info(f'Downloading query output "{target}"...')
@@ -57,13 +57,43 @@ def download_query(target):
 
     job.set_public(get_make_public(target))
 
-    target = Path(TXT_ROOT, target)
-    target.parent.mkdir(parents=True, exist_ok=True)
-    with target.open(mode='w') as fh:
+    target_path = Path(TXT_ROOT, target)
+    target_path.parent.mkdir(parents=True, exist_ok=True)
+    with target_path.open(mode='w') as fh:
         fh.write(job.output())
-    if target.stat().st_size != int(job.output_size()):
-        logger.warning(f"Downloaded output of {target} is {target.stat().st_size}, should be {job.output_size()}, deleting.")
-        target.unlink()
+    if target_path.stat().st_size != int(job.output_size()):
+        logger.warning(f"Downloaded output of {target} is {target_path.stat().st_size}, should be {job.output_size()}, deleting.")
+        target_path.unlink()
+
+def verifyDownload(target):
+    target_path = Path(TXT_ROOT, target)
+
+    if not target_path.exists():
+        return False
+
+    job_data = get_query_data()
+
+    client = get_client()
+    job = client.get_job(job_data[target]['job'])
+
+    actual_size = target_path.stat().st_size
+    expected_size = int(job.output_size())
+    if actual_size != expected_size:
+        logger.warning(f"Downloaded output of {target} is {actual_size}, should be {expected_size}, deleting.")
+        target_path.unlink()
+        return False
+
+    actual_hash = job.output_hash()
+    with target_path.open(mode='r') as fh:
+        data = fh.read(actual_hash[0])
+    expected_hash = md5(str.encode(data)).hexdigest()
+    if actual_hash[1] != expected_hash:
+        logger.warning(f"Downloaded output of {target} has bad hash, deleting.")
+        target_path.unlink()
+        return False
+
+    target_path.touch()
+    return True
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -81,11 +111,7 @@ if __name__ == '__main__':
     if is_run_needed(target):
         run_query(target)
 
-    target_path = Path(args.target)
-
-    if not target_path.exists():
+    if not verifyDownload(target):
         download_query(target)
-    else:
-        target_path.touch()
 
     close_client()
