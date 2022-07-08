4ba141c192d35b8ae671e8695a24f0ee82bfe062
Robert Dyer <rdyer@unl.edu>
2022-07-07 19:37:57 -0500

    escape file paths in case they have spaces

--- bin/build-makefile.py
+++ bin/build-makefile.py
@@ -17,6 +17,9 @@
 # limitations under the License.
 from utilities import get_query_config, build_replacements, TXT_ROOT, CSV_ROOT, PQ_ROOT, ANALYSIS_ROOT
 
+def escape(s):
+    return s.replace(' ', '\\ ')
+
 if __name__ == '__main__':
     configuration = get_query_config()
 
@@ -36,8 +39,8 @@ if __name__ == '__main__':
 
     for target in configuration['queries']:
         query_info = configuration['queries'][target]
-        substitution_files = [x for (_, x) in build_replacements(configuration.get('substitutions', []), query_info.get('substitutions', []), only_files = True)]
-        target = TXT_ROOT + target
+        substitution_files = [escape(x) for (_, x) in build_replacements(configuration.get('substitutions', []), query_info.get('substitutions', []), only_files = True)]
+        target = TXT_ROOT + escape(target)
         txt.append(target)
 
         clean_target = f'clean-{target}'
@@ -48,16 +51,16 @@ if __name__ == '__main__':
 
         if 'csv' in query_info and 'output' in query_info['csv']:
             csv_info = query_info['csv']
-            csv_output = CSV_ROOT + csv_info['output']
+            csv_output = CSV_ROOT + escape(csv_info['output'])
             csv.append(csv_output)
 
-            filename = csv_info['output'][:-4]
+            filename = escape(csv_info['output'][:-4])
             print('')
             print(f'{clean_target} += {csv_output}')
             print(f'{clean_target} += {PQ_ROOT}{filename}.parquet')
             print(f'{clean_target} += {PQ_ROOT}{filename}-deduped.parquet')
             print(f'{csv_output}: {target}')
-            print('\t@$(MKDIR) $(dir $@)')
+            print('\t@$(MKDIR) "$(dir $@)"')
             string = '\t$(PYTHON) $(BOATOCSV)'
             if 'test' in csv_info:
                 for test in csv_info['test']:
@@ -69,24 +72,24 @@ if __name__ == '__main__':
                 string += f' --header "{csv_info["header"]}"'
             if 'numidx' in csv_info:
                 string += f' --numidx {int(csv_info["index"])}'
-            string += ' $< > $@'
+            string += ' "$<" > "$@"'
             print(string)
             print(f'\t@$(RM) {PQ_ROOT}{filename}.parquet')
             print(f'\t@$(RM) {PQ_ROOT}{filename}-deduped.parquet')
 
         if 'gendupes' in query_info and 'output' in query_info['gendupes']:
             dupes_info = query_info['gendupes']
-            dupes_txt = TXT_ROOT + dupes_info['output']
+            dupes_txt = TXT_ROOT + escape(dupes_info['output'])
             txt.append(dupes_txt)
 
             print('')
             print(f'{clean_target} += {dupes_txt}')
             print(f'{dupes_txt}: {target} $(word 1, $(GENDUPES))')
-            print('\t@$(MKDIR) $(dir $@)')
-            print('\t$(PYTHON) $(GENDUPES) $< > $@')
+            print('\t@$(MKDIR) "$(dir $@)"')
+            print('\t$(PYTHON) $(GENDUPES) "$<" > "$@"')
 
             if 'csv' in dupes_info:
-                dupes_csv  = CSV_ROOT + dupes_info['csv']
+                dupes_csv  = CSV_ROOT + escape(dupes_info['csv'])
                 csv.append(dupes_csv)
 
                 print('')
@@ -94,16 +97,16 @@ if __name__ == '__main__':
                 print(f'{clean_target} += {PQ_ROOT}$**/dupes.parquet')
                 print(f'{clean_target} += {PQ_ROOT}$**/*-deduped.parquet')
                 print(f'{dupes_csv}: {dupes_txt}')
-                print('\t@$(MKDIR) $(dir $@)')
-                print('\t$(PYTHON) $(BOATOCSV) $< > $@')
+                print('\t@$(MKDIR) "$(dir $@)"')
+                print('\t$(PYTHON) $(BOATOCSV) "$<" > "$@"')
                 print(f'\t@$(RM) {PQ_ROOT}$**/dupes.parquet')
                 print(f'\t@$(RM) {PQ_ROOT}$**/*-deduped.parquet')
 
         print('')
-        string = f'boa/{query_info["query"]} '
-        string += ' '.join(substitution_files)
+        string = escape(f'boa/{query_info["query"]}')
+        string += ' ' + ' '.join(substitution_files)
         print(f'{target}: {string.strip()}')
-        print('\t$(PYTHON) $(DOWNLOAD) $@')
+        print('\t$(PYTHON) $(DOWNLOAD) "$@"')
         print('')
 
         print(f'.PHONY: {clean_target}')
@@ -119,11 +122,12 @@ if __name__ == '__main__':
     analyses = []
 
     for script in configuration['analyses']:
+        script = escape(script)
         target = script.split('.')[0]
         analyses.append(target)
 
         inputs = configuration['analyses'][script]['input']
-        inputs = [CSV_ROOT + x for x in inputs]
+        inputs = [CSV_ROOT + escape(x) for x in inputs]
 
         print('')
         print(f'{target}: {ANALYSIS_ROOT}{script} ' + ' '.join(inputs))
