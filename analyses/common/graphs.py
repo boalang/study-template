c488f3a503fc6696180c70d30eae8d5d23ac5616
Robert Dyer <rdyer@unl.edu>
2022-09-05 11:34:27 -0500

    avoid type 3 fonts in plots

--- analyses/common/graphs.py
+++ analyses/common/graphs.py
@@ -5,10 +5,15 @@ from matplotlib import pyplot as plt
 __all__ = ["setup_plots"]
 
 def setup_plots(rcParams = None):
+    # avoid Type 3 fonts
+    plt.rcParams['pdf.fonttype'] = 42
+    plt.rcParams['ps.fonttype'] = 42
+
     plt.rcParams['figure.figsize'] = [6.0, 5.0]
     plt.rcParams['figure.dpi'] = 600.0
     plt.rcParams['font.size'] = 24
     if rcParams:
         for (k,v) in rcParams.items():
             plt.rcParams[k] = v
+
     plt.subplots(constrained_layout=True)
