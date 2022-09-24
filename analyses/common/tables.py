9829c986f4c43ea0bbd0d9b02583beb92aab8e35
Robert Dyer <rdyer@unl.edu>
2022-09-24 15:58:29 -0500

    fix type hint name for Styler

--- analyses/common/tables.py
+++ analyses/common/tables.py
@@ -2,6 +2,7 @@
 
 import os
 import pandas as pd
+import pandas.io.formats.style
 import sys
 from typing import Optional, Union
 
@@ -14,25 +15,25 @@ __all__ = [
     "save_table",
     ]
 
-def get_styler(df: Union[pd.DataFrame, pd.Series]) -> pd.io.formats.style.Styler:
+def get_styler(df: Union[pd.DataFrame, pd.Series]) -> pandas.io.formats.style.Styler:
     if isinstance(df, pd.Series):
         return df.to_frame().style
     return df.style
 
-def highlight_cols(styler: pd.io.formats.style.Styler) -> pd.io.formats.style.Styler:
+def highlight_cols(styler: pandas.io.formats.style.Styler) -> pandas.io.formats.style.Styler:
     styler = styler.applymap_index(lambda x: 'textbf:--rwrap;', axis='columns')
     return styler.hide(names=True, axis='columns')
 
-def highlight_rows(styler: pd.io.formats.style.Styler) -> pd.io.formats.style.Styler:
+def highlight_rows(styler: pandas.io.formats.style.Styler) -> pandas.io.formats.style.Styler:
     styler = styler.applymap_index(lambda x: 'textbf:--rwrap;', axis='index')
     return styler.hide(names=True, axis='index')
 
 _colsepname = ''
-def save_table(styler: pd.io.formats.style.Styler, filename: str, subdir: Optional[str]=None, decimals: Optional[int]=2, thousands: Optional[str]=',', colsep: Optional[str]=None, **kwargs):
+def save_table(styler: pandas.io.formats.style.Styler, filename: str, subdir: Optional[str]=None, decimals: Optional[int]=2, thousands: Optional[str]=',', colsep: Optional[str]=None, **kwargs):
     '''Saves a DataFrame to a LaTeX table.
 
     Args:
-        styler (pd.io.formats.style.Styler): A Pandas Styler object for formatting a table.
+        styler (pandas.io.formats.style.Styler): A Pandas Styler object for formatting a table.
         filename (str): The filename to save to, including '.tex' extension. Files are saved under 'tables/'.
         subdir (Optional[str]): the sub-directory, underneath 'tables/', to save in. Defaults to None.
         decimals (Optional[int]): How many decimal places for floats. Defaults to 2.
