65f308828e573658b2283853f5a3d2f7b62b9246
Robert Dyer <rdyer@unl.edu>
2022-11-05 21:04:10 -0500

    update the get_df functions

--- analyses/common/df.py
+++ analyses/common/df.py
@@ -2,7 +2,7 @@
 
 import os
 import pandas as pd
-from typing import Optional, Union, List, Callable
+from typing import Optional, List, Callable
 
 from .utils import _resolve_dir, _get_dir
 
@@ -11,13 +11,14 @@ __all__ = [
     "get_deduped_df",
     ]
 
-def get_df(filename: str, subdir: Optional[str]=None, header: Optional[Union[List[int], bool, str]]=None, precache_function: Optional[Callable[[pd.DataFrame], pd.DataFrame]]=None, **kwargs) -> pd.DataFrame:
-    '''Loads a CSV file into a DataFrame.
+def get_df(filename: str, subdir: Optional[str]=None, drop: Optional[List[str]]=None, precache_function: Optional[Callable[[pd.DataFrame], pd.DataFrame]]=None, **kwargs) -> pd.DataFrame:
+    '''Loads a CSV file into a DataFrame. Extra keyword arguments are passed directly to read_csv.
 
     Args:
         filename (str): the CSV file to load, without the '.csv' extension
         subdir (Optional[str], optional): the sub-directory, underneath 'data/csv/', that it lives in. Defaults to None.
-        precache_function (Callable[DataFrame] -> DataFrame, optional): a function to modify the the data frame before caching.
+        drop (Optional[List[str]], optional): Any columns to drop from the table after loading. Defaults to None.
+        precache_function (Callable[DataFrame] -> DataFrame, optional): a function to modify the the data frame before caching. Defaults to None.
 
     Returns:
         pd.DataFrame: the CSV file as a Pandas DataFrame
@@ -25,14 +26,16 @@ def get_df(filename: str, subdir: Optional[str]=None, header: Optional[Union[Lis
     try:
         df = pd.read_parquet(_resolve_dir(f'data/parquet/{_get_dir(subdir)}{filename}.parquet'))
     except:
-        df = pd.read_csv(_resolve_dir(f'data/csv/{_get_dir(subdir)}{filename}.csv'), index_col=False, header=header, **kwargs)
+        df = pd.read_csv(_resolve_dir(f'data/csv/{_get_dir(subdir)}{filename}.csv'), index_col=False, **kwargs)
+        if drop:
+            df = df.drop(drop, axis=1)
         if precache_function:
             df = precache_function(df)
         os.makedirs(_resolve_dir(f'data/parquet/{_get_dir(subdir)}'), 0o755, True)
         df.to_parquet(_resolve_dir(f'data/parquet/{_get_dir(subdir)}{filename}.parquet'), compression='gzip')
     return df
 
-def get_deduped_df(filename: str, subdir: Optional[str]=None, ts: bool=False, **kwargs) -> pd.DataFrame:
+def get_deduped_df(filename: str, subdir: Optional[str]=None, drop: Optional[List[str]]=None, precache_function: Optional[Callable[[pd.DataFrame], pd.DataFrame]]=None, ts: bool=False, **kwargs) -> pd.DataFrame:
     '''Loads a CSV file into a DataFrame and de-duplicates the data.
 
     This function assumes your table has columns named 'project' and 'file', and no column named 'hash'.
@@ -40,6 +43,8 @@ def get_deduped_df(filename: str, subdir: Optional[str]=None, ts: bool=False, **
     Args:
         filename (str): the CSV file to load, without the '.csv' extension
         subdir (Optional[str], optional): the sub-directory, underneath 'data/csv/', that it lives in. Defaults to None.
+        drop (Optional[List[str]], optional): Any columns to drop from the table after loading. Defaults to None.
+        precache_function (Callable[DataFrame] -> DataFrame, optional): a function to modify the the data frame before caching. Defaults to None.
         ts (bool): if the hash file also has the file timestamps or not
 
     Returns:
@@ -49,9 +54,9 @@ def get_deduped_df(filename: str, subdir: Optional[str]=None, ts: bool=False, **
         df = pd.read_parquet(_resolve_dir(f'data/parquet/{_get_dir(subdir)}{filename}-deduped.parquet'))
     except:
         if ts:
-            df = _remove_dupes(get_df(filename, subdir, **kwargs), subdir, names=['var', 'hash', 'project', 'ts', 'file'])
+            df = _remove_dupes(get_df(filename, subdir, drop, precache_function, **kwargs), subdir, names=['var', 'hash', 'project', 'ts', 'file'])
         else:
-            df = _remove_dupes(get_df(filename, subdir, **kwargs), subdir)
+            df = _remove_dupes(get_df(filename, subdir, drop, precache_function, **kwargs), subdir)
         os.makedirs(_resolve_dir(f'data/parquet/{_get_dir(subdir)}'), 0o755, True)
         df.to_parquet(_resolve_dir(f'data/parquet/{_get_dir(subdir)}{filename}-deduped.parquet'), compression='gzip')
     return df
