7f28951f956c753cc06e20e7b1575a3a5551a104
Robert Dyer <rdyer@unl.edu>
2023-11-21 20:56:18 -0600

    add get_dataset() utility function

--- analyses/common/utils.py
+++ analyses/common/utils.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 
+import json
 import os
 from typing import Optional
 
 
 __all__ = [
-    "_resolve_dir",
-    "_get_dir",
+    '_resolve_dir',
+    '_get_dir',
+    'get_dataset',
 ]
 
 def _resolve_dir(dir: str) -> str:
@@ -19,3 +21,14 @@ def _get_dir(subdir: Optional[str]) -> str:
     if subdir is None:
         return ''
     return subdir + '/'
+
+def get_dataset(filename: str, subdir: Optional[str]=None) -> str:
+    study_config = _resolve_dir('study-config.json')
+    with open(study_config) as f:
+        study_config = json.load(f)
+
+    datasets = study_config['datasets']
+    queries = study_config['queries']
+    query = queries[f'{_get_dir(subdir)}{filename}.txt']
+
+    return datasets[query['dataset']]
